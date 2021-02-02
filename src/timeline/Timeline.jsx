406e62139b10d8c486c4ea283139cd1eab88afb7
Benjamin Cloquet <benjamin.cloquet@gmail.com>
2021-02-02 16:59:38 +0100

    Add more timeline events

--- src/timeline/Timeline.jsx
+++ src/timeline/Timeline.jsx
@@ -19,7 +19,7 @@ const renderTimelineEvents = () => [
     <p className="timeline__event__description">
       I learned software engineering, management and
       {' '}
-      <a href="blank">the ins and outs of making a video game in 48 hours.</a>
+      <a href="https://kevcouscous.itch.io/koro-unijam2017">the ins and outs of making a video game in 48 hours.</a>
     </p>
   </div>,
   <div id="event2" key="event2-content">
@@ -28,6 +28,45 @@ const renderTimelineEvents = () => [
       <LocationIcon />
       <h2>Universiti Teknologi PETRONAS, Malaysia</h2>
     </div>
+    <p className="timeline__event__description">
+      I collaborated with machine learning researchers and developed a
+      {' '}
+      <a href="https://github.com/kyoshiro10500/GA-Viz">data visualization app for genetic algorithms.</a>
+    </p>
+  </div>,
+  <div id="event3" key="event3-content">
+    <h1 className="timeline__event__title">Ubisoft</h1>
+    <div className="timeline__event__location">
+      <LocationIcon />
+      <h2>Montpellier, France</h2>
+    </div>
+    <p className="timeline__event__description">
+      I implemented gameplay mechanics in C++ on
+      {' '}
+      {' '}
+      <a href="https://www.youtube.com/watch?v=BLWt9MQLVgU">Tom Clancy&apos;s Ghost Recon Breakpoint</a>
+      {' '}
+      during the beta, debugging and post-launch cycles.
+      {' '}
+      <br />
+      I also worked on AI features for
+      {' '}
+      <a href="https://www.youtube.com/watch?v=CC3GA2ddWFU">Riders Republic.</a>
+    </p>
+  </div>,
+  <div id="event4" key="event4-content">
+    <h1 className="timeline__event__title">Full-stack web projects</h1>
+    <div className="timeline__event__location">
+      <LocationIcon />
+      <h2>Montpellier, France</h2>
+    </div>
+    <p className="timeline__event__description">
+      I made the best of lockdown and learned Javascript from the bottom up,
+      eventually picking my stack :
+      React, Node, Express and MongoDB.
+      <br />
+      Take a look at my projects below!
+    </p>
   </div>,
 ];
 
