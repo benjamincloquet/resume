12b1b9055f7dd0bf313ecbfb600a4287c7587f50
Benjamin Cloquet <benjamin.cloquet@gmail.com>
2021-02-09 14:19:36 +0100

    Adjust About section line height

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
       </div>
       <animated.p className="about__text" style={textPerspectiveStyle}>
         <animated.h1 className="about__title" style={titlePerspectiveStyle}>This is me!</animated.h1>
-        I&apos;ve been walking the earth for 24 years old and fiddling with code for almost half of that.
+        I&apos;ve been walking the earth for 24 years and fiddling with code for almost half of that.
         <br />
         I&apos;m also a huge fan of board games, basketball, techno music and that first sip of beer after a long week.
       </animated.p>
