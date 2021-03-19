c8feeae3b76711c63063d015818c64edd7dc7fd2
Benjamin Cloquet <benjamin.cloquet@gmail.com>
2021-03-19 16:59:02 +0100

    Make header responsive

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const About = () => (
   <section className="w-full py-40 flex flex-col justify-start items-center bg-green space-y-8 skew">
-    <div className="container w-7/12 flex flex-col space-y-8">
+    <div className="container w-5/6 sm:w-7/12 flex flex-col space-y-8">
       <h1 className="text-black text-7xl self-start">This is me !</h1>
-      <div className="container w-full flex flex-row items-center space-x-12">
-        <div className="flex flex-col items-center space-y-2 flex-none transform hover:-translate-x-2 hover:-translate-y-2 transition ease-in-out duration-500">
-          <img className="w-72 border-4 border-green-dark" src={`${process.env.PUBLIC_URL}/normandie.png`} alt="benjamin cloquet" />
+      <div className="container w-full flex flex-col sm:flex-row items-center sm:space-x-12">
+        <div className="flex flex-col items-center space-y-2 flex-none transform hover:-translate-x-2 hover:-translate-y-2 transition ease-in-out duration-500 mb-6 sm:mb-0">
+          <img className="w-3/4 sm:w-72 border-4 border-green-dark" src={`${process.env.PUBLIC_URL}/normandie.png`} alt="benjamin cloquet" />
           <legend className="text-2xl">fig. 1 : me</legend>
         </div>
         <div className="flex flex-col space-y-8 bg-white border-4 border-green-dark border-solid text-justify bg-white py-8 px-8 self-stretch transform hover:-translate-x-2 hover:-translate-y-2 transition ease-in-out duration-500">
--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -9,14 +9,14 @@ const Header = () => (
     >
       <h6 className="px-1 text-3xl text-white difference lowercase">Benjamin Cloquet</h6>
     </a>
-    <InteractiveText className="flex-grow self-stretch">
+    <InteractiveText className="flex-grow self-stretch text-center">
       <h1 key="greeting" className="text-9xl text-green text-opacity-50 text-stroke-black-thin">Hello World</h1>
       <h1 key="greeting" className="text-9xl text-green text-opacity-70 text-stroke-black-thin">Hello World</h1>
       <h1 key="greeting" className="text-9xl text-green text-opacity-100 text-stroke-black-thin">Hello World</h1>
     </InteractiveText>
     <div className="flex flex-col items-center">
       <h1 className="text-3xl mb-8 lowercase">Scroll to learn about me !</h1>
-      <svg className="w-6 animate-bounce" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <svg className="w-12 sm:w-6 animate-bounce" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
       </svg>
     </div>
