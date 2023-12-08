700f210fecff32ab95c94b4c2ccb85234192277f
Benjamin Cloquet <benjamin.cloquet@econocom.com>
2023-12-08 13:58:06 +0100

    fix: asset prefix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   // output: process.env.NODE_ENV === 'production' ? 'export' : 'standalone',
   // basePath: process.env.NODE_ENV === 'production' ? '/resume' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/resume' : '',
   output: 'export',
   images: {
     unoptimized: true
