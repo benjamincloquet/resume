9d40e93a5e29ac974fdc48e3cf443ea60ef53211
Benjamin Cloquet <benjamin.cloquet@econocom.com>
2023-12-08 11:39:08 +0100

    fix: asset prefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: process.env.NODE_ENV === 'production' ? 'export' : 'standalone'
+  output: process.env.NODE_ENV === 'production' ? 'export' : 'standalone',
+  assetPrefix: process.env.NODE_ENV === "production" ? "/resume/" : undefined,
 }
 
 module.exports = nextConfig
