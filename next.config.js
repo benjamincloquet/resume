52b948450017b5707bd584abf64fc6679c75d85e
Benjamin Cloquet <benjamin.cloquet@econocom.com>
2023-12-08 13:54:38 +0100

    feat: try unoptimized images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: process.env.NODE_ENV === 'production' ? 'export' : 'standalone',
-  basePath: process.env.NODE_ENV === 'production' ? '/resume' : '',
+  // output: process.env.NODE_ENV === 'production' ? 'export' : 'standalone',
+  // basePath: process.env.NODE_ENV === 'production' ? '/resume' : '',
+  output: 'export',
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
